5a33f10c8a99b2936c9df47dcb0c6cbea143ad51
Fiona <fiona.greenwood.sheridan@gmail.com>
2020-10-05 21:13:34 +1100

    Comment stuff!

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ dotenv.config();
 const sqlite3 = require("sqlite3");
 const { open } = require("sqlite");
 
+//Initalize Team Cache
 const NodeCache = require("node-cache");
 const teamCache = new NodeCache();
 async function initalizeCache(){
@@ -23,53 +24,72 @@ initalizeCache();
 const { Client } = require("discord.js");
 const client = new Client({disableMentions:"all"});
 
+//Command Handler
 client.on("message",async (message)=>{
+    //reject nonapplicable messages
     if(message.author.bot) return;
     if(message.guild?.id != process.env.GUILD) return;
     if(!message.content.startsWith(process.env.PREFIX)) return;
 
+    //Grab the arguments and command from the message
     let args = message.content.substr(process.env.PREFIX.length).trim().split(" ");
     let command = args.shift();
     
+    //if they just did the prefix, get outta there
     if(!command) return;
     
+    //Set greeting command
     if(command.toLowerCase() == "setgreeting"){
+        //Get the current team the user is on
         let roles = message.member.roles.cache.map(r=>r.id);
         let teams = teamCache.mget(roles);
         let teamData = teams[Object.keys(teams)[0]];
 
         if(!teamData) return message.channel.send("You are not yet part of a team");
         
+        //Check to see if they are allowed to set the greeting
         if(!message.member.roles.cache.has(teamData.admin)) return message.channel.send("You do not have permission to do that!");
 
+        //Update the db with the new info
         let db = await open({
             filename: "data.db",
             driver: sqlite3.Database
         });
 
         await db.run("update teams set message = ?1 where admin = ?2", args.join(" "), teamData.admin);
+
+        //Confirm Change
         message.channel.send("Greeting Updated");
+
+        //Refresh Cache
         initalizeCache();
     }
+
+    //Test Greeting Command
     else if(command.toLowerCase() == "testgreeting"){
+        //Find users team
         let roles = message.member.roles.cache.map(r=>r.id);
         let teams = teamCache.mget(roles);
         let teamData = teams[Object.keys(teams)[0]];
 
         if(!teamData) return message.channel.send("You are not yet part of a team");
         
+        //Prepare the message by replacing neccissary parts
         let greeting = teamData.message;
         greeting = greeting.replace(/NAME/g, message.member.displayName);
 
+        //Attempt to send the message
         message.member.send(greeting).catch(error=>{switch(error.message){
         case "Cannot send messages to this user": 
-            message.reply("Your DMs are closed");
+            message.reply("Your DMs are closed"); //If the bot is blocked, or the person has allow dms off
             break;
         default: 
-            message.reply("Something went wrong");
+            message.reply("Something went wrong"); //whoops
             console.error(error);
         }});
     }
+
+    //Help command, it is what it looks like!
     else if(command.toLowerCase() == "help"){
         message.channel.send(
             `> **Info**
@@ -87,34 +107,44 @@ Displays this message.
 
 });
 
+//Team Change Handling
 client.on("guildMemberUpdate",(oldMember,newMember)=>{
+    // Not of this guild? Need not apply
     if(newMember.guild.id != process.env.GUILD) return;
+
+    //Grab the change in roles
     let differenceRoles = oldMember.roles.cache.difference(newMember.roles.cache);
     let oldRole = oldMember.roles.cache.intersect(differenceRoles).first();
     let newRole = newMember.roles.cache.intersect(differenceRoles).first();
+
+    //Log the change in roles (Debugging Purposes)
     if(newRole) console.log(`${newMember.displayName}: + ${newRole.name}`);
     if(oldRole) console.log(`${newMember.displayName}: - ${oldRole.name}`);
 
+    //If they didn't recieve a new role, then we are done here
     if(newRole == undefined) return;
     
+    //Find the team of the new role, if there is none, why even bother?
     let teamData = teamCache.get(newRole.id);
     if(!teamData) return;
 
+    //Check that they have been on the server within the appropriate time frame
     let joinDate = newMember.joinedAt;
     let checkDate = new Date();
     checkDate.setHours(checkDate.getHours()-process.env.JOINLENGTH);
 
     if(joinDate < checkDate) return;
 
-    //Templater
+    //Replace appropriate key words to make message more personallized
     let greeting = teamData.message;
     greeting = greeting.replace(/NAME/g, newMember.displayName);
 
+    //Attempt to greet
     newMember.send(greeting).catch(error=>{switch(error.message){
     case "Cannot send messages to this user": 
-        console.log(`Tried to greet ${newMember.displayName}, but their DMs are disabled!`);
+        console.log(`Tried to greet ${newMember.displayName}, but their DMs are disabled!`); //it's 😔 okay
         break;
-    default: console.error(error);
+    default: console.error(error); //it's 😬 okay
     }});
 
 });
